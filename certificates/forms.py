0d18da3bd6cd27c322027d59239c58973b2ede53
ant0shik <anton.korotkix@gmail.com>
2018-05-23 14:24:16 +0300

    Validate domain name by Django RegexValidator

--- certificates/forms.py
+++ certificates/forms.py
@@ -1,31 +1,25 @@
-import re
-
-#from django.core.exceptions import ValidationError
+from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
-from django.utils.translation import gettext_lazy as _
 
 from django import forms
 from django.forms import Textarea
 
 from .models import *
 
-# Validation domain name
-#def validate_domain(input_domain):
-#    if not re.match('^((?=[a-z0-9-]{1,63}\.)([a-z0-9]+|[a-z0-9][a-z0-9-]*[a-z0-9])*\.)+([a-z]|xn--[a-z0-9-]+){2,63}$', input_domain.replace('*.', '') ):
-#        raise ValidationError(
-#            _('%(input_domain)s is incorrect domain name'),
-#            params={'input_domain': input_domain},
-#        )
 
 class DomainForm(forms.Form):
-    #domain_name = forms.CharField(widget=forms.TextInput, label='Enter domen name', validators=[validate_domain], error_messages={'invalid': _(u'Enter a valid domain name.')})
     domain_name = forms.CharField(
         widget=forms.TextInput,
         label='Enter domain name',
-        error_messages={'invalid_domain_name': 'Domain name not match regex'},
-        validators=[RegexValidator(r'^((?=[a-z0-9-]{1,63}\.)([a-z0-9]+|[a-z0-9][a-z0-9-]*[a-z0-9])*\.)+([a-z]|xn--[a-z0-9-]+){2,63}$', 'Enter a valid domain name')]
-    )
-     
+        required=True,
+        validators=[
+            RegexValidator(
+                regex = r'^((?=[a-z0-9-]{1,63}\.)([a-z0-9]+|[a-z0-9][a-z0-9-]*[a-z0-9])*\.)+([a-z]|xn--[a-z0-9-]+){2,63}$',
+                message = 'Invalid domain name',
+                code = 'invalid_domain_name'
+            )
+        ]
+    )     
 
 class ManageForm(forms.Form):
     private_key = forms.CharField(label="PK")
