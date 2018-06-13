6e045bd8bb84e81c03456aec341a08c5970d3f85
Литвинов Илья Константинович <ilya.k.litvinov@tele2.ru>
2018-06-13 14:01:02 +0300

    Add RemoteUserBackend

--- PKI/settings.py
+++ PKI/settings.py
@@ -47,13 +47,13 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django_remote_auth_ldap.middleware.RemoteUserMiddleware'
+    'django.contrib.auth.middleware.RemoteUserMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 AUTHENTICATION_BACKENDS = [
-    'django_remote_auth_ldap.backend.RemoteUserLDAPBackend',
+    'django.contrib.auth.backends.RemoteUserBackend',
 ]
 
 ROOT_URLCONF = 'PKI.urls'
